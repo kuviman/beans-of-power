c9efcd563c4204cb0a98025ef275d48fcf71a955
kuviman <kuviman@gmail.com>
2022-11-02 01:12:54 +0400

    Remove unnecessary files

--- snippets/geng-f466ac77a7f2d621/inline0.js
+++
@@ -1,7 +0,0 @@
-
-    export function show_error(text) {
-        document.getElementById("geng-canvas").style.display = "none";
-        document.getElementById("error-message").textContent = text;
-        document.getElementById("geng-error-screen").style.display = "block";
-    }
-    
